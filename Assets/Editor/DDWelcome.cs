f3d581387b027385c139948925ca755dff6ffd79
陈柏财 <chenbocai@aobi.com>
2018-04-16 21:02:11 +0800

    创建数据驱动编辑器

---
+++ Assets/Editor/DDWelcome.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace DataDriven
+{
+    /// <summary>
+    /// 数据驱动编辑器的欢迎界面
+    /// </summary>
+    public static class DDWelcome
+    {
+        private static string Fliter = "txt";
+        /// <summary>
+        /// 渲染窗口
+        /// </summary>
+        public static void OnGUI(DataDrivenEditor _editor) {
+            Vector2 size = _editor.position.size;
+            float h = size.y * 0.5f;
+            float w = size.x * 0.5f;
+            bool isPress = GUI.Button(new Rect(new Vector2(w-50, h-70), new Vector2(100, 60)), new GUIContent("打开"));
+            if (isPress) {
+                string path = EditorUtility.OpenFilePanel("选择您要打开的文件", _editor.ConfigPath, Fliter);
+                if (path != null && path.Length > 0)
+                {
+
+                }
+                else
+                {
+                    _editor.ShowNotification(new GUIContent("用户取消操作"));
+                }
+            }
+            isPress = GUI.Button(new Rect(new Vector2(w -50, h+10), new Vector2(100, 60)), new GUIContent("创建"));
+            if (isPress)
+            {
+                string defaultName = System.DateTime.Now.Ticks.ToString();
+                string path = EditorUtility.SaveFilePanelInProject("您要保存的文件", defaultName, Fliter, "message");
+                if (path != null && path.Length > 0)
+                {
+                    string _p = Application.dataPath.Replace("Assets", "") + path;
+                    File.CreateText(_p);
+                    //Debug.Log(Application.dataPath + path);
+                }
+                else
+                {
+                    _editor.ShowNotification(new GUIContent("用户取消操作"));
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Editor/DataDrivenEditor.cs
@@ -0,0 +1,77 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DataDriven
+{
+    /// <summary>
+    /// 数据驱动编辑工具
+    /// </summary>
+    public class DataDrivenEditor : EditorWindow
+    {
+        /// <summary>
+        /// 配置路径
+        /// </summary>
+        public static string CONFIG_PATH = "";
+        /// <summary>
+        /// 窗口标题
+        /// </summary>
+        private static string WINDOW_TITLE = "DataDriven编辑器";
+        /// <summary>
+        /// 窗口单例
+        /// </summary>
+        private static DataDrivenEditor _Instance = null;
+        /// <summary>
+        /// 编辑器状态枚举
+        /// </summary>
+        private enum EditorStatus {
+            /// <summary>
+            /// 欢迎界面
+            /// </summary>
+            Welcome,
+        }
+        /// <summary>
+        /// 当前编辑器的状态
+        /// </summary>
+        private EditorStatus _status = EditorStatus.Welcome;
+        /// <summary>
+        /// 构建编辑器窗口
+        /// </summary>
+        [MenuItem("Tools/DataDriven %1")]
+        static void CreateDataDrivenWindow() {
+            if (_Instance == null) {
+                _Instance = GetWindow<DataDrivenEditor>();
+                _Instance.titleContent = new GUIContent(WINDOW_TITLE);
+                _Instance.Init();
+            }
+            // 展示窗口
+            _Instance.Show();
+            // 聚焦展示
+            _Instance.Focus();
+        }
+        /// <summary>
+        /// 初始化编辑器
+        /// </summary>
+        private void Init() {
+            ConfigPath = Application.dataPath + CONFIG_PATH;
+        }
+        /// <summary>
+        /// gui渲染
+        /// </summary>
+        private void OnGUI()
+        {
+            if (_status == EditorStatus.Welcome) {
+                DDWelcome.OnGUI(this);
+            }
+            
+        }
+
+        #region 属性获取
+        /// <summary>
+        /// 配置路径
+        /// </summary>
+        public string ConfigPath {
+            get;set;
+        }
+        #endregion
+    }
+}
